4179bdf1b89610fc79c10a67e66f3912f7071152
zhanggi3 <gio.zhang@oocl.com>
2019-07-15 19:27:37 +0800

    [test]:[add should_get_25050_when_given_quantity_is_499_AND_itemPrice_is_50 ]

---
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -0,0 +1,21 @@
+package com.tws.refactoring.extract_variable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PriceCalculatorTest {
+
+  @Test
+  public void should_get_25050_when_given_quantity_is_499_AND_itemPrice_is_50(){
+    //given
+      int itemPrice = 50;
+      int quantity = 499;
+      PriceCalculator priceCalculator = new PriceCalculator();
+    //when
+      double result = priceCalculator.getPrice(quantity,itemPrice);
+    //then
+    assertEquals(25050.0,result,0.01);
+  }
+
+}
