9f710fd507bad4a119d1368e80919cdbdd64fd97
zhanggi3 <gio.zhang@oocl.com>
2019-07-15 19:29:07 +0800

    [refactor]:[refactor PriceCalculator class by breaking up into multiple methods ]

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -4,11 +4,24 @@ public class PriceCalculator {
     private final int BASE_COUNT = 500;
     private final double DISCOUNT=0.05;
     private final double ITEM_DISCOUNT = 0.01;
-    private final double MAX_NUM=100;
+    private final double FREE_SHIPING_ON=100;
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
         return quantity * itemPrice -
-                Math.max(0, quantity - BASE_COUNT) * itemPrice * DISCOUNT +
-                Math.min(quantity * itemPrice * ITEM_DISCOUNT, MAX_NUM);
+            Math.max(0, calculateBasePrice(quantity)) * calculateItemPriceDiscount(itemPrice) +
+                Math.min(calculateItemDiscount(quantity, itemPrice), FREE_SHIPING_ON);
+    }
+
+    private double calculateItemDiscount(int quantity, int itemPrice) {
+        return quantity * itemPrice * ITEM_DISCOUNT;
+    }
+
+
+    private double calculateItemPriceDiscount(int itemPrice) {
+        return itemPrice * DISCOUNT;
+    }
+
+    private int calculateBasePrice(int quantity) {
+        return quantity - BASE_COUNT;
     }
 }
